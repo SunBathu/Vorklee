c336c2d2342ee6c2bfe567032954db9ff7e45e50
SunBathu <sunbathu@gmail.com>
2024-12-09 12:20:05 +0530

    just test

--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function SettingsPage() {
-  // ----------------------------
+  // -----------------------------
   // State for Global Settings
   // ----------------------------
   const [globalSettings, setGlobalSettings] = useState({
